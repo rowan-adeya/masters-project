21f7d1f6c033d47709a756e9af19415a76cdaad9
rowan-adeya <rowan_adeya@yahoo.co.uk>
2025-06-25 17:17:24 +0100

    Adds error-handling to the simulator class.

--- Research Project/Code/TLSQHOSimulator.py
+++ Research Project/Code/TLSQHOSimulator.py
@@ -10,10 +10,10 @@ class TLSQHOSimulator:
     plot and save the results within the repository itself.
 
     Instance attributes:
-        H(q.core.qobj.Qobj): Hamiltonian which governs evolution dynamics.
-        psi0(q.core.qobj.Qobj): Initial state at t=0 from which the system evolves.
-        L(q.core.qobj.Qobj): Optional, Jump operator which governs decay of Open Quantum Systems.
-        e_ops(list(q.core.qobj.Qobj)): Optional, list of projectors to calculate expectation values from.
+        H(q.Qobj): Hamiltonian which governs evolution dynamics.
+        psi0(q.Qobj): Initial state at t=0 from which the system evolves.
+        L(q.Qobj): Optional, Jump operator which governs decay of Open Quantum Systems.
+        e_ops(list(q.Qobj)): Optional, list of projectors to calculate expectation values from.
         times(np.ndarray): Optional, numpy array of times for which to evolve the initial state from.
 
 
@@ -28,28 +28,49 @@ class TLSQHOSimulator:
 
     def __init__(
         self,
-        H: q.core.qobj.Qobj,
-        psi0: q.core.qobj.Qobj,
-        L: q.core.qobj.Qobj = None,
+        H: q.Qobj,
+        psi0: q.Qobj,
+        L: q.Qobj = None,
         e_ops: list = None,
         times: np.ndarray = None,
     ):
+        if not (H.type == "oper" and isinstance(H, q.Qobj)):
+            raise TypeError("The Hamiltonian must be a QuTip Qobj of type 'oper'.")
         self.H = H
+
+        if not (psi0.type == "ket" and isinstance(psi0, q.Qobj)):
+            raise TypeError("The initial state must be a QuTip Qobj of type 'ket'.")
         self.psi0 = psi0
 
         if L is None:
             self.L = []
         else:
+            if not isinstance(L, list):
+                L = [L]
+            for op in L:
+                if not (op.type == "oper" and isinstance(op, q.Qobj)):
+                    raise TypeError(
+                        "The jump operator must be a QuTip Qobj of type 'oper'."
+                    )
             self.L = L
 
         if times is None:
             self.times = np.linspace(0.0, 20.0, 200)
         else:
+            if not isinstance(times, np.ndarray):
+                raise TypeError("The list of times must be a NumPy array.")
             self.times = times
 
         if e_ops is None:
             self.e_ops = []
         else:
+            if not isinstance(e_ops, list):
+                e_ops = [e_ops]
+            for op in e_ops:
+                if not (op.type == "oper" and isinstance(op, q.Qobj)):
+                    raise TypeError(
+                        "Each expectation operator must be a QuTip Qobj of type 'oper'."
+                    )
             self.e_ops = e_ops
 
     def evolve(self):
@@ -75,10 +96,13 @@ class TLSQHOSimulator:
             results(q.solver.result.Result): Output of evolve() method, which is a solver type for QuTip.
 
         """
-        if self.e_ops is None:
-            raise Exception(
+        if not self.e_ops:
+            raise ValueError(
                 "Cannot provide expectation values if no projectors are specified."
             )
+        if results.states == []:
+            raise ValueError("No states were found in the results object.")
+        
         return results.expect
 
     def vne(self, results: q.solver.result.Result):
@@ -88,6 +112,9 @@ class TLSQHOSimulator:
         Args:
             results(q.solver.result.Result): Output of evolve() method, which is a solver type for QuTip.
         """
+        if results.states == []:
+            raise ValueError("No states were found in the results object.")
+        
         atom_subsys = [state.ptrace(0) for state in results.states]
         return [q.entropy_vn(dm) for dm in atom_subsys]
 
@@ -99,6 +126,9 @@ class TLSQHOSimulator:
             results(q.solver.result.Result): Output of evolve() method, which is a solver type for QuTip.
 
         """
+        if results.states == []:
+            raise ValueError("No states were found in the results object.")
+        
         coherence = []
 
         for ket in results.states:
@@ -127,14 +157,14 @@ class TLSQHOSimulator:
         Plots data and saves to specific location in repository.
 
         Args:
-            y_data(list) : A list/array of data which the user plots against times. It should be the outputs 
+            y_data(list) : A list/array of data which the user plots against times. It should be the outputs
             of vne(), rel_coherence() and expect() methods of this class.
             title(str) : Name of graph.
             y_label(str) : Y-axis label.
             filename(str) : Name of file which will be saved.
-            legend(list) : A list of strings which must be provided if at least one expectation operator is 
-            present and the user implements the expect() method. 
-                        
+            legend(list) : A list of strings which must be provided if at least one expectation operator is
+            present and the user implements the expect() method.
+
 
         """
         savepath = f"results/{filename}"
