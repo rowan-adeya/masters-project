abef8e71eb27766be63160a97c3647d7c8d18c21
rowan-adeya <rowan_adeya@yahoo.co.uk>
2025-06-26 15:26:18 +0100

    Adds simulations for TLS and QHO decay in OQS approach.

--- Research Project/Code/JCM_simulation.py
+++ Research Project/Code/JCM_simulation.py
@@ -1,12 +1,12 @@
 from TLSQHOSimulator import TLSQHOSimulator
 import qutip as q
 import numpy as np
+from scipy.constants import k as k_B
 
 # constants
 w = 1.0
 g = 1.0
 N = 2
-gamma = np.sqrt(0.1)
 
 # Bases
 basis_atom_e = q.basis(2, 0)
@@ -32,33 +32,67 @@ results_closed = sim_closed.evolve()
 vne_closed = sim_closed.vne(results_closed)
 coherence_closed = sim_closed.rel_coherence(results_closed)
 
-# Open Dynamics
-L = gamma * s_lower
+# Open Dynamics (Spontaneous Emission)
+gamma = np.sqrt(0.1)
+
+L_tls = gamma * s_lower
 times_open = np.linspace(0.0, 500.0, 200)
 e_ops = [adag * a, s_raise * s_lower]
-sim_open = TLSQHOSimulator(H, psi0, L, e_ops, times_open)
-results_open = sim_open.evolve()
-expect_open = sim_open.expect(results_open)
+sim_open_tls = TLSQHOSimulator(H, psi0, L_tls, e_ops, times_open)
+results_open_tls = sim_open_tls.evolve()
+expect_open_tls = sim_open_tls.expect(results_open_tls)
+
+# Open Dynamics (QHO Photon Loss)
+gamma_th = 0.1
+T = 300  # avg T in Kelvin
+n_omega = 1 / (np.exp(w / (k_B * T)) - 1)
+kappa = gamma_th * (1 + n_omega)
+
+L_qho = kappa * a
+sim_open_qho = TLSQHOSimulator(H, psi0, L_qho, e_ops, times_open)
+results_open_qho = sim_open_qho.evolve()
+expect_open_qho = sim_open_qho.expect(results_open_qho)
+
+L_tlsqho = [L_qho, L_tls]
+sim_open_tlsqho = TLSQHOSimulator(H, psi0, L_tlsqho, e_ops, times=np.linspace(0.0, 2000.0, 200))
+results_open_tlsqho = sim_open_tlsqho.evolve()
+expect_open_tlsqho = sim_open_tlsqho.expect(results_open_tlsqho)
 
 
 # Plots
 sim_closed.plot(
     vne_closed,
-    "Entanglement Oscillations For Closed Dynamics in the JCM",
+    "Closed System Evolution of the JCM: Entanglement Oscillations",
     "Entanglement",
     "JCM_CQS_ent",
 )
 sim_closed.plot(
     coherence_closed,
-    "Coherence Oscillations For Closed Dynamics in the JCM",
+    "Closed System Evolution of the JCM: Coherence Oscillations",
     "Coherence",
     "JCM_CQS_coh",
 )
 
-sim_open.plot(
-    expect_open,
-    "Expectation Values of Atom and QHO for Open Dynamics in the JCM",
+sim_open_tls.plot(
+    expect_open_tls,
+    "Open System Evolution of the JCM: Dynamics of Spontaneous Atomic Emission",
     "Expectation Values",
-    "JCM_OQS_expect",
+    "JCM_OQS_TLS_Decay",
+    ["cavity photon number", "atom excitation probability"],
+)
+
+sim_open_qho.plot(
+    expect_open_qho,
+    "Open System Evolution of the JCM: Dynamics of QHO Photon Loss",
+    "Expectation Values",
+    "JCM_OQS_QHO_loss",
+    ["cavity photon number", "atom excitation probability"],
+)
+
+sim_open_tlsqho.plot(
+    expect_open_tlsqho,
+    "Open System Evolution of the JCM: Dynamics of TLS, QHO Decay",
+    "Expectation Values",
+    "JCM_OQS_QHOTLS",
     ["cavity photon number", "atom excitation probability"],
 )
